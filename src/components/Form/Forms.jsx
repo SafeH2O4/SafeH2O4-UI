d4582436a92f5438b3374149d8fd256929fdeed6
Aparna0224 <aparna6024@gmail.com>
2024-06-11 17:46:02 +0530

    Gh pages

--- src/components/Form/Forms.jsx
+++ src/components/Form/Forms.jsx
@@ -28,19 +28,19 @@ function Formp({ closeForm, productName }) {
 
         <div className="body">
           <form onSubmit={handleSubmit(onSubmit)}>
-            <label class="input-group__label" htmlFor="cusName">Customer Name</label>
+            <label className="input-group__label" htmlFor="cusName">Customer Name</label>
             <input 
-             class="input-group__input"
+             className="input-group__input"
               type="text" 
               id="cusName" 
               {...register("cusName", { required: "Customer name is required" })} 
             />
-            <span class="input-group__error"> {errors.cusName && <p className="error">{errors.cusName.message}</p>}</span>
+            <span className="input-group__error"> {errors.cusName && <p className="error">{errors.cusName.message}</p>}</span>
 
 
-            <label class="input-group__label" htmlFor="cusPhoneNum">Phone Number</label>
+            <label className="input-group__label" htmlFor="cusPhoneNum">Phone Number</label>
             <input 
-              class="input-group__input"
+              className="input-group__input"
               type="text" 
               id="cusPhoneNum" 
               {...register("cusPhoneNum", { 
@@ -51,15 +51,15 @@ function Formp({ closeForm, productName }) {
                 }
               })} 
             />
-            <span class="input-group__error"> {errors.cusPhoneNum && <p className="error">{errors.cusPhoneNum.message}</p>}</span>
+            <span className="input-group__error"> {errors.cusPhoneNum && <p className="error">{errors.cusPhoneNum.message}</p>}</span>
         
 
-            <label class="input-group__label" htmlFor="email">Email Id</label>
+            <label className="input-group__label" htmlFor="email">Email Id</label>
             <input 
-              class="input-group__input"
+              className="input-group__input"
               type="email" 
               id="email" 
-              placeholder="SAFEH2O4@gamil.com"
+              placeholder="SAFEH2O4@gmail.com"
               {...register("email", { 
                 required: "Email is required",
                 pattern: {
@@ -68,12 +68,12 @@ function Formp({ closeForm, productName }) {
                 }
               })} 
             />
-            <span class="input-group__error"> {errors.email && <p className="error">{errors.email.message}</p>}</span>
+            <span className="input-group__error"> {errors.email && <p className="error">{errors.email.message}</p>}</span>
       
 
-            <label class="input-group__label" htmlFor="product">Product Requested:</label>
+            <label className="input-group__label" htmlFor="product">Product Requested:</label>
             <input 
-              class="input-group__input"
+              className="input-group__input"
               type="text" 
               id="product" 
               value={productName} 
@@ -82,9 +82,9 @@ function Formp({ closeForm, productName }) {
             />
             
 
-            <label class="input-group__label" htmlFor="quantity">Quantity</label>
+            <label className="input-group__label" htmlFor="quantity">Quantity</label>
             <input 
-              class="input-group__input"
+              className="input-group__input"
               type="number" 
               id="quantity" 
               {...register("quantity", { 
@@ -93,17 +93,17 @@ function Formp({ closeForm, productName }) {
                 max: { value: 10, message: "Maximum quantity is 10" }
               })} 
             />
-            <span class="input-group__error"> {errors.quantity && <p className="error">{errors.quantity.message}</p>}</span>
+            <span className="input-group__error"> {errors.quantity && <p className="error">{errors.quantity.message}</p>}</span>
             
 
-            <label class="input-group__label" htmlFor="date">Preferred Delivery Date</label>
+            <label className="input-group__label" htmlFor="date">Preferred Delivery Date</label>
             <input
-              class="input-group__input" 
+              className="input-group__input" 
               type="date" 
               id="date" 
               {...register("date", { required: "Preferred delivery date is required" })} 
             />
-            <span class="input-group__error"> {errors.date && <p className="error">{errors.date.message}</p>}</span>
+            <span className="input-group__error"> {errors.date && <p className="error">{errors.date.message}</p>}</span>
           
 
             <div className="footer">
