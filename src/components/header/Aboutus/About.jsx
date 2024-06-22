4795f05168a123b5214c67df8d4415496da687c8
Darish05 <153887116+Darish05@users.noreply.github.com>
2024-06-22 15:45:16 +0530

    changes

--- src/components/header/Aboutus/About.jsx
+++ src/components/header/Aboutus/About.jsx
@@ -9,21 +9,21 @@ const About = () => {
       <div className="aboutcontent">
         <h1>About us</h1>
         <p>
-          Welcome to Safe H20, where we are dedicated to providing you with the
+          Welcome to Safe H2O4, where we are dedicated to providing you with the
           cleanest, healthiest, and safest drinking water. Our mission is to
           ensure every household has access to pure water, free from
           contaminants and impurities. With years of expertise and a passion for
           innovation, we offer a range of state-of-the-art water purification
           systems tailored to meet the diverse needs of our customers. At Safe
-          H20, we believe that access to clean water is a fundamental right. Our
-          team of experts works tirelessly to develop advanced filtration
+          H2O4, we believe that access to clean water is a fundamental right.
+          Our team of experts works tirelessly to develop advanced filtration
           technologies that remove harmful substances while preserving essential
           minerals, ensuring that every drop is not only safe but also
           refreshing and beneficial to your health. Our commitment to quality
           and customer satisfaction drives us to deliver products that are
           reliable, efficient, and environmentally friendly. Join us on our
           journey towards a healthier lifestyle and a cleaner planet. Choose
-          Safe H20 for your home and experience the difference that pure, clean
+          Safe H2O4 for your home and experience the difference that pure, clean
           water can make. Thank you for trusting us with your water purification
           needs.
         </p>
